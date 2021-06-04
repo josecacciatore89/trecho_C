4cc32866ddbed921bb8f9f028f5a93dfc0cd345b
josecacciatore89 <60232884+josecacciatore89@users.noreply.github.com>
2021-06-04 19:52:20 -0300

    Add files via upload

---
+++ menu.c
@@ -0,0 +1,44 @@
+#include"quarto.hpp"
+#include <iostream>
+#include "reserva.hpp"
+
+using namespace std;
+
+int main() {
+
+int option = 1;
+
+		cout << "\n1-: "<<endl;
+		cout << "2-: "<<endl;
+		cout << "3-: "<<endl;
+		cout << "4-: "<<endl;
+		cout << "5-: "<<endl;
+		cout << "0-Sair: " << endl;
+
+		cin >> option;
+
+    cout << "\n\n";
+
+    switch(option){
+      case 1:
+      
+      break;
+
+      case 2:
+			
+			break;
+
+      case 3:
+			
+			break;
+
+      case 4:
+			
+			break;
+      
+      case 5:
+			
+			break;
+    }
+    
+}
